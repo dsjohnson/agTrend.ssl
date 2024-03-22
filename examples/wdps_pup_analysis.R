6bb4ecfa713f7c9ed0410ea846e94fb511e66a7e
Devin Johnson <devin.johnson@noaa.gov>
2024-03-22 12:51:51 -0600

    nothing new

--- examples/wdps_pup_analysis.R
+++ examples/wdps_pup_analysis.R
@@ -15,7 +15,7 @@ site.info <- wdpspup %>% select(site, region, rca) %>% distinct()
 ### Fit Tweedie hierarchical GAM model for used for imputation
 fit <- fit.gam(data=wdpspup, obl.corr=FALSE,
                alt.mod=list(
-                 mu.form = count ~  s(year, k=8) + s(year, site, bs="fs", k=8)
+                 mu.form = count ~  s(year, site, bs="fs", k=8, m=1)
                )
                )
 
@@ -25,21 +25,21 @@ N <- sample.abund(fit, wdpspup, yrs=1989:2019, size=10000, add.site.data=site.in
 N.summ <- ag.summary(N, ci.prob=0.9)
 
 ### Run the section below to plot all sites
-# for(i in 1:nrow(N)){
-#   df <- filter(N.summ, site==N$site[i])
-#   p <- ggplot(data=df) +
-#     geom_ribbon(aes(x=year, ymin=ci.pred.lower, ymax=ci.pred.upper), alpha=0.2) +
-#     geom_path(aes(x=year, y=est.pred), alpha=1, color='darkred', lwd=1.2) +
-#     geom_point(aes(x=year, y=est.real), data=df %>% filter(!is.na(count)))+
-#     ggtitle(df$site[[1]]) + xlab('Year') + ylab('Count')
-#   print(p)
-# }
+for(i in 1:nrow(N)){
+  df <- filter(N.summ, site==N$site[i])
+  p <- ggplot(data=df) +
+    geom_ribbon(aes(x=year, ymin=ci.pred.lower, ymax=ci.pred.upper), alpha=0.2) +
+    geom_path(aes(x=year, y=est.pred), alpha=1, color='darkred', lwd=1.2) +
+    geom_point(aes(x=year, y=est.real), data=df %>% filter(!is.na(count)))+
+    ggtitle(df$site[[1]]) + xlab('Year') + ylab('Count')
+  print(p)
+}
 
 reg.N <- ag.abund(N, 'region')
 reg.summ <- ag.summary(reg.N)
 reg.tr <- ag.trend(reg.N, timeframe=c(2000,2019), ci.prob=0.95)
 
-reg.tr$growth %>% arrange(type, rca)
+reg.tr$growth %>% arrange(type, region)
 
 ### Plot of fit
 ggplot(data=reg.summ) +
@@ -48,7 +48,7 @@ ggplot(data=reg.summ) +
   geom_path(aes(x=year, y=Est), data=reg.tr$fitted %>% filter(type=='predicted'), color='darkred', lwd=1.2) +
   geom_ribbon(aes(x=year, ymin=lower, ymax=upper), reg.tr$fitted %>% filter(type=='predicted'), alpha=0.2, fill='darkred') +
   geom_pointrange(aes(x=year, y=est.real, ymin=ci.real.lower, ymax=ci.real.upper), data=reg.summ %>% filter(surveyed==1)) +
-  xlab('Year') + ylab('Count') + facet_wrap(~reg, nrow=3, scale='free_y')
+  xlab('Year') + ylab('Count') + facet_wrap(~region, nrow=3, scale='free_y')
 
 wdps.N <- N %>% mutate(total="wDPS") %>% ag.abund( 'total')
 wdps.summ <- ag.summary(wdps.N)
